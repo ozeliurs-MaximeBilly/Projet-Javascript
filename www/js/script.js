0cd6bfad1ac7f0dc5ac1aae1ffd742d296a9eb8c
BILLY Maxime <b20022165@A-F001719.salsa.univ-amu.fr>
2022-02-21 10:42:06 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- www/js/script.js
+++ www/js/script.js
@@ -1,1 +1,25 @@
-
+const ctx = document.getElementById('UnCharted').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6'],
+        datasets: [{
+            label: 'Temperature Int',
+            data: [12, 19, 3, 5, 2, 3],
+            borderColor: '#9b59b6'
+        },
+        {
+            label: 'Temperature Ext',
+            data: [0, 1, 2, 3, 4, 5],
+            borderColor: '#2c3e50'
+        },
+    ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
