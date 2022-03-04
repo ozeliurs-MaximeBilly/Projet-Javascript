c1e973804e84c5666339fe6a5fc984879698dd9e
ozeliurs <ozeliurs@gmail.com>
2022-03-04 12:10:23 +0100

    C'est le bordel

--- www/js/script.js
+++ www/js/script.js
@@ -1,29 +1,31 @@
-setInterval(update, 1000);
+setInterval(update, 5000);
+
+hist = {}
 
 const socket = new WebSocket('wss://ws.hothothot.dog:9502');
 
-socket.addEventListener('open', function (event) {
-    socket.send('Hello HotHotHot');
-   });
+socket.onopen = function (event) {
+    socket.send('Hello HotHotHot, I am hothothot.ozeliurs.com');
+}
 
-socket.addEventListener('message', function (event) {
-    console.log('Voici un message du serveur', event.data);
-});
+socket.onmessage = function(event) {
+    console.log(event.data)
+}
 
 const ctx = document.getElementById('UnCharted').getContext('2d');
 const UnCharted = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['1', '2', '3', '4', '5', '6'],
+        labels: [],
         datasets: [{
             label: 'Temperature Int',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [],
             borderColor: '#9b59b6',
             tension: 0.3
         },
         {
             label: 'Temperature Ext',
-            data: [0, 1, 2, 3, 4, 5],
+            data: [],
             borderColor: '#2c3e50',
             tension: 0.3
         },
@@ -38,7 +40,7 @@ const UnCharted = new Chart(ctx, {
     }
 });
 
-var hist = {"time":[] ,"temp_ext":[], "temp_int":[]}
+var hist = {}
 
 function affHist() {
     document.getElementById("now-container").style.display = 'none';
@@ -57,39 +59,101 @@ function getRandomInt(min, max) {
 
 function getData() {
     return {
-        "temp_ext":{
-            "timestamp": Date.now(),
-            "value": getRandomInt(-10, 50)
-        },
-        "temp_int":{
-            "timestamp": Date.now(),
-            "value": getRandomInt(10, 30)
-        }
+        "HotHotHot":"Api v1.0",
+        "capteurs":[
+            {
+                "type":"Thermique",
+                "Nom":"interieur",
+                "Valeur": getRandomInt(-10, 50),
+                "Timestamp": Date.now()
+            },
+            {
+                "type":"Thermique",
+                "Nom":"exterieur",
+                "Valeur": getRandomInt(-10, 50),
+                "Timestamp": Date.now()
+            }
+        ]
     }
+    // return {
+    //     "temp_ext":{
+    //         "timestamp": Date.now(),
+    //         "value": getRandomInt(-10, 50)
+    //     },
+    //     "temp_int":{
+    //         "timestamp": Date.now(),
+    //         "value": getRandomInt(10, 30)
+    //     }
+    // }
 }
 
+// function fetch() {
+//     fetch("https://hothothot.dog/api/capteurs")
+//         .then(response => response.json())
+//         .then(data => );
+//     return response
+// }
+
 function update(event) {
-    let data = getData();
-    hist["time"].push( new Date( data["temp_ext"]["timestamp"]  ).toLocaleTimeString()  )
-    hist["temp_ext"].push(data["temp_ext"]["value"])
-    hist["temp_int"].push(data["temp_int"]["value"])
+    console.log(socket.readyState)
+    // if (socket.readyState == 1) {
+    //     var data = {}
+    // } else {
+    //     var data = getData();
+    // }
 
-    UnCharted.data.labels = hist["time"]
-    UnCharted.data.datasets[0].data = hist["temp_int"];
-    UnCharted.data.datasets[1].data = hist["temp_ext"];
+    var data = getData();
+    console.log(data)
+    data.capteurs.forEach(capt => {
 
-    document.getElementById("temp-ext").getElementsByTagName("h1")[0].innerHTML = data["temp_ext"]["value"] + "°C";
-    document.getElementById("temp-ext").getElementsByTagName("b")[0].innerHTML = Math.min.apply(Math, hist["temp_ext"]) + "°C";
-    document.getElementById("temp-ext").getElementsByTagName("b")[1].innerHTML = Math.max.apply(Math, hist["temp_ext"]) + "°C";
+        // Ajout d'element si pas de element existant
+        if (document.getElementById(capt.Nom) == null) {
+
+            template = document.getElementById("temp-template")
+            clone = document.importNode(template.content, true)
+            
+            clone.firstElementChild.setAttribute("id", capt.Nom)
+            clone.getElementById("title").innerHTML = capt.Nom;
+            clone.getElementById("value").innerHTML = capt.Valeur;
+
+            var container = document.getElementById("now-container")
+            container.appendChild(clone)
+        }
+
+        // Historique
+        if (!capt.Nom in hist) {
+            hist[capt.Nom] = []
+        }
+        hist[capt.Nom].push(capt.Valeur)
+        
+        // Population des elements
+        document.getElementById(capt.Nom).getElementsByTagName("h1")[0].innerHTML = capt.Valeur + "°C";
+        document.getElementById(capt.Nom).getElementsByTagName("b")[0].innerHTML = Math.min.apply(Math, hist[capt.Nom]) + "°C";
+        document.getElementById(capt.Nom).getElementsByTagName("b")[1].innerHTML = Math.max.apply(Math, hist[capt.Nom]) + "°C";
+    });
+
+    console.log(hist)
+
+    // hist["time"].push( new Date( data["temp_ext"]["timestamp"] ).toLocaleTimeString() )
+    // hist["temp_ext"].push(data["temp_ext"]["value"])
+    // hist["temp_int"].push(data["temp_int"]["value"])
+
+    // UnCharted.data.labels = hist["time"]
+    // UnCharted.data.datasets[0].data = hist["temp_int"];
+    // UnCharted.data.datasets[1].data = hist["temp_ext"];
+
+    // document.getElementById("temp-ext").getElementsByTagName("h1")[0].innerHTML = data["temp_ext"]["value"] + "°C";
+    // document.getElementById("temp-ext").getElementsByTagName("b")[0].innerHTML = Math.min.apply(Math, hist["temp_ext"]) + "°C";
+    // document.getElementById("temp-ext").getElementsByTagName("b")[1].innerHTML = Math.max.apply(Math, hist["temp_ext"]) + "°C";
     
 
-    Math.max(hist["temp_int"])
-    Math.min(hist["temp_int"])
-    document.getElementById("temp-int").getElementsByTagName("h1")[0].innerHTML = data["temp_int"]["value"] + "°C";
-    document.getElementById("temp-int").getElementsByTagName("b")[0].innerHTML = Math.min.apply(Math, hist["temp_int"]) + "°C";
-    document.getElementById("temp-int").getElementsByTagName("b")[1].innerHTML = Math.max.apply(Math, hist["temp_int"]) + "°C";
+    // Math.max(hist["temp_int"])
+    // Math.min(hist["temp_int"])
+    // document.getElementById("temp-int").getElementsByTagName("h1")[0].innerHTML = data["temp_int"]["value"] + "°C";
+    // document.getElementById("temp-int").getElementsByTagName("b")[0].innerHTML = Math.min.apply(Math, hist["temp_int"]) + "°C";
+    // document.getElementById("temp-int").getElementsByTagName("b")[1].innerHTML = Math.max.apply(Math, hist["temp_int"]) + "°C";
 
-    UnCharted.update();
+    // UnCharted.update();
 }
 
 document.getElementById("now").addEventListener("click", affNow);
