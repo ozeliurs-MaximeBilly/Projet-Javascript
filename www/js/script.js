42479c17708ab1cd32113241c3ae1ef5c05c5955
ozeliurs <ozeliurs@gmail.com>
2022-03-11 09:15:04 +0100

    Readme + notifications + fake data

--- www/js/script.js
+++ www/js/script.js
@@ -76,10 +76,12 @@ function updateTempLabels(data) {
         if (data.Valeur > 35){
             document.getElementById("exterieur").getElementsByTagName("h5")[1].style.visibility = "visible";
             document.getElementById("exterieur").getElementsByTagName("h5")[1].innerHTML = "Hot Hot Hot !";
+            notify("Hot Hot Hot !")
             document.getElementById("exterieur").getElementsByTagName("h5")[1].style.color = "red";
         } else if (data.Valeur < 0) {
             document.getElementById("exterieur").getElementsByTagName("h5")[1].style.visibility = "visible";
             document.getElementById("exterieur").getElementsByTagName("h5")[1].innerHTML = "Banquise en vue !";
+            notify("Banquise en vue !")
             document.getElementById("exterieur").getElementsByTagName("h5")[1].style.color = "blue";
         } else {
             document.getElementById("exterieur").getElementsByTagName("h5")[1].style.visibility = "hidden";
@@ -91,19 +93,23 @@ function updateTempLabels(data) {
         if (data.Valeur > 22 && data.Valeur <= 50) {
             document.getElementById("interieur").getElementsByTagName("h5")[1].style.visibility = "visible";
             document.getElementById("interieur").getElementsByTagName("h5")[1].innerHTML = "Baissez le chauffage !";
+            notify("Baissez le chauffage !")
             document.getElementById("interieur").getElementsByTagName("h5")[1].style.color = "orange";
         }
         else if (data.Valeur > 50) {
             document.getElementById("interieur").getElementsByTagName("h5")[1].style.visibility = "visible";
             document.getElementById("interieur").getElementsByTagName("h5")[1].innerHTML = "Appelez les pompiers ou arrêtez votre barbecue !";
+            notify("Appelez les pompiers ou arrêtez votre barbecue !")
             document.getElementById("interieur").getElementsByTagName("h5")[1].style.color = "red";
         } else if (data.Valeur < 12 && data.Valeur >= 0) {
             document.getElementById("interieur").getElementsByTagName("h5")[1].style.visibility = "visible";
             document.getElementById("interieur").getElementsByTagName("h5")[1].innerHTML = "Montez le chauffage ou mettez un gros pull  !";
+            notify("Montez le chauffage ou mettez un gros pull  !")
             document.getElementById("interieur").getElementsByTagName("h5")[1].style.color = "cyan";
         } else if (data.Valeur < 0) {
             document.getElementById("interieur").getElementsByTagName("h5")[1].style.visibility = "visible";
             document.getElementById("interieur").getElementsByTagName("h5")[1].innerHTML = "Canalisations gelées, appelez SOS plombier et mettez un bonnet !";
+            notify("Canalisations gelées, appelez SOS plombier et mettez un bonnet !")
             document.getElementById("interieur").getElementsByTagName("h5")[1].style.color = "blue";
         }
         else {
@@ -141,6 +147,36 @@ document.getElementById("hist").addEventListener("click", event => {
     document.getElementById("hist").classList = "button primary";
     document.getElementById("now").classList = "button";
 });
+
+document.getElementById("notif").addEventListener("click", event => {
+
+    if (!('Notification' in window)) {
+        console.log('Ce navigateur ne prend pas en charge la notification de bureau')
+    }
+    
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission()
+        .then((permission) => {
+            if (permission === 'granted') {
+                const notification = new Notification('Hothothot', {
+                    body: 'Notifications activées !',
+                    icon: '/icons/webIcon.png'
+                })
+            }
+        })
+    }
+})
+
+function notify(message) {
+    if (!('Notification' in window)) { return }
+    
+    if (Notification.permission !== 'granted') { return }
+
+    new Notification('Hothothot', {
+        body: message,
+        icon: '/icons/webIcon.png'
+    })
+}
 
 
 // Chart Config
@@ -249,23 +285,23 @@ function updateDisplay(data) {
     UnCharted.update()
 }
 
-function update(event) { // called every 5 seconds checks if wss fails
+function update(event) { // called every 10 seconds checks if wss fails
     
-    if (no_data_timeout < 5) { // bypass wss if not responding
-        if (no_data_detect == 0) {
-            no_data_timeout += 1
-        }
+    // if (no_data_timeout < 5) { // bypass wss if not responding
+    //     if (no_data_detect == 0) {
+    //         no_data_timeout += 1
+    //     }
 
-        if (socket.readyState == 1) {
-            return
-        }
-    }
+    //     if (socket.readyState == 1) {
+    //         return
+    //     }
+    // }
 
     // if wss fails get back with fetch
-    fetchAPIandUpdate();
+    // fetchAPIandUpdate();
     
-    // var data = getData(); // Fake data
-    // updateDisplay(data);
+    var data = getData(); // Fake data
+    updateDisplay(data);
 }
 
 update()
